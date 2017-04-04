5816f08e79c62ba6e550fcb162e4e15ef03b5976
Tony Bussieres <tony.bussieres@ticksmith.com>
2017-04-04 17:22:27 -0400

    update build.sh, was not copying the final static properly

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
+mkdir /tmp/haproxy ||:
 mkdir /tmp/work
 cd /tmp/work
 git clone https://gist.github.com/24fab751202dff4d547c.git buildHAProxy
 cd /tmp/work/buildHAProxy
 ./buildHAProxy.sh
+cp -r /tmp/work/buildHAProxy/work/target/ /tmp/haproxy/
